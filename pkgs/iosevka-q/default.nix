c883d1a475a43c213c2fdff058bb8a5705c65c7b
Ludovico Piero <lewdovico@gnuweeb.org>
2024-06-02 17:01:07 +0900

    iosevka-q: patch with nerd-font-patcher
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- pkgs/iosevka-q/default.nix
+++ pkgs/iosevka-q/default.nix
@@ -6,6 +6,7 @@
   darwin,
   remarshal,
   ttfautohint-nox,
+  nerd-font-patcher,
   privateBuildPlan ? ''
     [buildPlans.IosevkaQ]
     family = "Iosevka q"
@@ -124,98 +125,104 @@
 }:
 assert (privateBuildPlan != null) -> set != null;
 assert (extraParameters != null) -> set != null;
-  buildNpmPackage rec {
-    pname =
-      if set != null
-      then "Iosevka${set}"
-      else "Iosevka";
-    version = "30.1.2";
+buildNpmPackage rec {
+  pname = if set != null then "Iosevka${set}" else "Iosevka";
+  version = "30.1.2";
 
-    src = fetchFromGitHub {
-      owner = "be5invis";
-      repo = "iosevka";
-      rev = "v${version}";
-      hash = "sha256-8NzzGF2DP4WaUg17Z5cpp6K71xs2rOudUkGb5S9TpsU=";
-    };
+  src = fetchFromGitHub {
+    owner = "be5invis";
+    repo = "iosevka";
+    rev = "v${version}";
+    hash = "sha256-8NzzGF2DP4WaUg17Z5cpp6K71xs2rOudUkGb5S9TpsU=";
+  };
 
-    npmDepsHash = "sha256-vjfNNwVoML57FJq14lH17OiM8N0xjbTiOe1RbW6ZRj0=";
+  npmDepsHash = "sha256-vjfNNwVoML57FJq14lH17OiM8N0xjbTiOe1RbW6ZRj0=";
 
-    nativeBuildInputs =
-      [
-        remarshal
-        ttfautohint-nox
-      ]
-      ++ lib.optionals stdenv.isDarwin [
-        # libtool
-        darwin.cctools
-      ];
+  nativeBuildInputs =
+    [
+      remarshal
+      ttfautohint-nox
+      nerd-font-patcher
+    ]
+    ++ lib.optionals stdenv.isDarwin [
+      # libtool
+      darwin.cctools
+    ];
 
-    buildPlan =
-      if builtins.isAttrs privateBuildPlan
-      then builtins.toJSON {buildPlans.${pname} = privateBuildPlan;}
-      else privateBuildPlan;
+  buildPlan =
+    if builtins.isAttrs privateBuildPlan then
+      builtins.toJSON { buildPlans.${pname} = privateBuildPlan; }
+    else
+      privateBuildPlan;
 
-    inherit extraParameters;
-    passAsFile =
-      ["extraParameters"]
-      ++ lib.optionals (!(builtins.isString privateBuildPlan && lib.hasPrefix builtins.storeDir privateBuildPlan)) ["buildPlan"];
+  inherit extraParameters;
+  passAsFile =
+    [ "extraParameters" ]
+    ++ lib.optionals (
+      !(builtins.isString privateBuildPlan && lib.hasPrefix builtins.storeDir privateBuildPlan)
+    ) [ "buildPlan" ];
 
-    configurePhase = ''
-      runHook preConfigure
-      ${lib.optionalString (builtins.isAttrs privateBuildPlan) ''
-        remarshal -i "$buildPlanPath" -o private-build-plans.toml -if json -of toml
-      ''}
-      ${
-        lib.optionalString
-        (builtins.isString privateBuildPlan && (!lib.hasPrefix builtins.storeDir privateBuildPlan))
-        ''
-          cp "$buildPlanPath" private-build-plans.toml
-        ''
-      }
-      ${
-        lib.optionalString
-        (builtins.isString privateBuildPlan && (lib.hasPrefix builtins.storeDir privateBuildPlan))
-        ''
-          cp "$buildPlan" private-build-plans.toml
-        ''
-      }
-      ${lib.optionalString (extraParameters != null) ''
-        echo -e "\n" >> params/parameters.toml
-        cat "$extraParametersPath" >> params/parameters.toml
-      ''}
-      runHook postConfigure
+  configurePhase = ''
+    runHook preConfigure
+    ${lib.optionalString (builtins.isAttrs privateBuildPlan) ''
+      remarshal -i "$buildPlanPath" -o private-build-plans.toml -if json -of toml
+    ''}
+    ${lib.optionalString
+      (builtins.isString privateBuildPlan && (!lib.hasPrefix builtins.storeDir privateBuildPlan))
+      ''
+        cp "$buildPlanPath" private-build-plans.toml
+      ''
+    }
+    ${lib.optionalString
+      (builtins.isString privateBuildPlan && (lib.hasPrefix builtins.storeDir privateBuildPlan))
+      ''
+        cp "$buildPlan" private-build-plans.toml
+      ''
+    }
+    ${lib.optionalString (extraParameters != null) ''
+      echo -e "\n" >> params/parameters.toml
+      cat "$extraParametersPath" >> params/parameters.toml
+    ''}
+    runHook postConfigure
+  '';
+
+  buildPhase = ''
+    export HOME=$TMPDIR
+    runHook preBuild
+
+    npm run build --no-update-notifier --targets ttf::$pname -- --jCmd=$NIX_BUILD_CORES --verbose=9 ttf::$pname
+
+    # Patch the font with nerd-font-patcher
+    cd "dist/$pname/TTF"
+    find . -type f -name "*.ttf" | xargs -P $NIX_BUILD_CORES -I {} nerd-font-patcher -c -s {}
+    cd ../../../
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    fontdir="$out/share/fonts/truetype"
+    install -d "$fontdir"
+    install "dist/$pname/TTF"/* "$fontdir"
+
+    runHook postInstall
+  '';
+
+  enableParallelBuilding = true;
+
+  meta = with lib; {
+    homepage = "https://typeof.net/Iosevka/";
+    downloadPage = "https://github.com/be5invis/Iosevka/releases";
+    description = "Versatile typeface for code, from code.";
+    longDescription = ''
+      Iosevka is an open-source, sans-serif + slab-serif, monospace +
+      quasi‑proportional typeface family, designed for writing code, using in
+      terminals, and preparing technical documents.
     '';
-
-    buildPhase = ''
-      export HOME=$TMPDIR
-      runHook preBuild
-      npm run build --no-update-notifier --targets ttf::$pname -- --jCmd=$NIX_BUILD_CORES --verbose=9 ttf::$pname
-      runHook postBuild
-    '';
-
-    installPhase = ''
-      runHook preInstall
-
-      fontdir="$out/share/fonts/truetype"
-      install -d "$fontdir"
-      install "dist/$pname/TTF"/* "$fontdir"
-
-      runHook postInstall
-    '';
-
-    enableParallelBuilding = true;
-
-    meta = with lib; {
-      homepage = "https://typeof.net/Iosevka/";
-      downloadPage = "https://github.com/be5invis/Iosevka/releases";
-      description = "Versatile typeface for code, from code.";
-      longDescription = ''
-        Iosevka is an open-source, sans-serif + slab-serif, monospace +
-        quasi‑proportional typeface family, designed for writing code, using in
-        terminals, and preparing technical documents.
-      '';
-      license = licenses.ofl;
-      platforms = platforms.all;
-      maintainers = with maintainers; [ludovicopiero];
-    };
-  }
+    license = licenses.ofl;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ ludovicopiero ];
+  };
+}

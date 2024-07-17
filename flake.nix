94e20dd0e67bd81c8fa63bdbda85258ab2046c0a
Ludovico Piero <lewdovico@gnuweeb.org>
2024-07-17 22:50:54 +0900

    wezterm: use rust nightly-rust from rust-overlay
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- flake.nix
+++ flake.nix
@@ -4,15 +4,19 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [
-        ./pkgs
-      ];
+      imports = [./pkgs];
 
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
       perSystem = {
         config,
@@ -25,6 +29,7 @@
         # This sets `pkgs` to a nixpkgs with allowUnfree option set.
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
+          overlays = [inputs.rust-overlay.overlays.default];
           config.allowUnfree = true;
         };
 

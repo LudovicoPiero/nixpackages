f07952e66dc29990666c34be069e6d54feddedcb
Maximilian Sforza <maximiliansforza@proton.me>
2024-08-11 17:09:28 +0000

    sarasa-gothic: 1.0.17 -> 1.0.18
    Diff: https://github.com/be5invis/Sarasa-Gothic/compare/None...v1.0.18

--- pkgs/sarasa-gothic/default.nix
+++ pkgs/sarasa-gothic/default.nix
@@ -6,13 +6,13 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "sarasa-gothic";
-  version = "1.0.17";
+  version = "1.0.18";
 
   src = fetchurl {
     # Use the 'ttc' files here for a smaller closure size.
     # (Using 'ttf' files gives a closure size about 15x larger, as of November 2021.)
     url = "https://github.com/be5invis/Sarasa-Gothic/releases/download/v${version}/Sarasa-TTC-${version}.7z";
-    hash = "sha256-Y6byateQu7R+8e9XWddLUy4xjU2WCWIOCykRzTWxIe4=";
+    hash = "sha256-Z4zTkcIDZdHmCrDMLlyTIAyarsPjQeF8Pbm1aRX1q+I=";
   };
 
   sourceRoot = ".";
